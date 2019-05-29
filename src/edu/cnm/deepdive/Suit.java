b46c62f6bd486735ea2c96396eb7fcd7592a1d99
Terrell Page <tpage46@gmail.com>
2019-05-29 10:55:03 -0600

    initial commit

---
+++ src/edu/cnm/deepdive/Suit.java
@@ -0,0 +1,39 @@
+package edu.cnm.deepdive;
+
+public enum Suit {
+
+  CLUBS,
+  DIAMONDS,
+  HEARTS,
+  SPADES;
+
+  public String abbreviation() {
+    switch (this) {
+      case CLUBS:
+        return "C";
+      case HEARTS:
+        return "H";
+      case SPADES:
+        return "S";
+      case DIAMONDS:
+        return "D";
+      default:
+        return null;
+    }
+  }
+
+  public Color getColor() {
+    switch (this) {
+      case CLUBS:
+      case SPADES:
+        return Color.BLACK;
+      default:
+        return Color.RED;
+    }
+  }
+
+  public enum Color {
+    RED, BLACK
+  }
+
+}
